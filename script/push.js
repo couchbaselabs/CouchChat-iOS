f6725109f154f7f55c2e546c797a9dece90f0eee
Chris Anderson <jchris@apache.org>
2013-02-21 17:24:53 -0800

    the script to push the design doc (pointed at "new" userapi http root)

---
+++ script/push.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+var config = require("./config"),
+  wrangler = require("sync-wrangler");
+
+config.channelmap =
+
+
+
+function(doc) {
+  if (doc.channel_id) {
+    sync("ch-"+doc.channel_id);
+  }
+  if (doc.channel_id == doc._id) {
+    // grant access to the channel to all
+    // members and owners
+    sync("ch-"+doc._id);
+    access(doc.owners, "ch-"+doc._id);
+    access(doc.members, "ch-"+doc._id);
+  }
+}
+
+.toString()
+
+config.validate_doc_update = function (newDoc, oldDoc, userCtx) {
+  if (newDoc._deleted === true && userCtx.roles.indexOf('_admin') === -1) {
+    throw "Only admin can delete documents on this database.";
+  }
+}.toString()
+
+wrangler.setup(config, function(err){
+  if (err) {return console.log("error"+ err)}
+  console.log("gateway configured")
+});
+
+

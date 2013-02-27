72b08a04a9cade3e54604fd04ce39e4a8e333526
Chris Anderson <jchris@apache.org>
2013-02-27 11:43:45 -0800

    consolidate to one sync function

--- script/push.js
+++ script/push.js
@@ -22,20 +22,6 @@ config.sync = function(doc, oldDoc, userCtx, secObj) {
 
 .toString()
 
-config.validate_doc_update = function (newDoc, oldDoc, userCtx) {
-  var doc = newDoc;
-  if (doc.channel_id && doc.channel_id == doc._id) {
-    // magic document, treat it carefully
-    // grants access to the channel to all members and owners
-    if (doc.owners.indexOf(userCtx.name) == -1) {
-      throw({unauthorized:"you are not a channel owner"});
-    }
-  }
-
-  if (newDoc._deleted === true && userCtx.roles.indexOf('_admin') === -1) {
-    throw "Only admin can delete documents on this database.";
-  }
-}.toString()
 
 wrangler.setup(config, function(err){
   if (err) {return console.log("error"+ err)}
